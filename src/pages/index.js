24e7103379b17e07505ae18cac551e7e2f111b9b
Daniel Shiffman <daniel.shiffman@gmail.com>
2024-04-23 18:30:29 -0400

    edits and updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
-import * as React from "react"
-import Layout from "../layout/Layout"
-import Hero from '../layout/Hero'
-import ScrollDownIndicator from "../components/ScrollDownIndicator";
-import TextBox from "../components/TextBox";
-import ProjectBox from "../components/ProjectBox";
-import Spacer from "../components/Spacer";
-import IframeBgComponent from "../components/IframeBgComponent";
+import * as React from 'react';
+import Layout from '../layout/Layout';
+import Hero from '../layout/Hero';
+import ScrollDownIndicator from '../components/ScrollDownIndicator';
+import TextBox from '../components/TextBox';
+import ProjectBox from '../components/ProjectBox';
+import Spacer from '../components/Spacer';
+import IframeBgComponent from '../components/IframeBgComponent';
 import { graphql } from 'gatsby';
 
 const IndexPage = ({ data }) => {
@@ -13,12 +13,12 @@ const IndexPage = ({ data }) => {
 
   const styles = {
     flexContainer: {
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      flexWrap: "wrap",
-    }
-  }
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexWrap: 'wrap',
+    },
+  };
 
   return (
     // if you plan to update header or footer, check the "Layout.js" file in "layout" folder.
@@ -33,54 +33,28 @@ const IndexPage = ({ data }) => {
       </section>
       <Spacer height="3rem" />
       <section aria-label="Introduction to ml5.js">
-        <h2>
-          Why ml5.js?
-        </h2>
+        <h2>Why ml5.js?</h2>
         <TextBox
           title='"More Approachable Machine Learning for the Web"'
           content="ml5.js aims to make machine learning approachable for a broad audience of artists, creative coders, and students. The library provides access to machine learning algorithms and models in the browser, building on top of TensorFlow.js with no other external dependencies."
           headingLevel={3}
         />
         <Spacer />
-        <h2>
-          What we can do with ml5.js?
-        </h2>
+        <h2>What you can do with ml5.js?</h2>
         <div style={styles.flexContainer}>
-          <ProjectBox
-            title="BodyPose"
-            description="Description"
-          />
-          <ProjectBox
-            title="HandPose"
-            description="Description"
-          />
-          <ProjectBox
-            title="FaceMesh"
-            description="Description"
-          />
-          <ProjectBox
-            title="Image Classifier"
-            description="Description"
-          />
-          <ProjectBox
-            title="Sound Classifier"
-            description="Description"
-          />
-          <ProjectBox
-            title="ml5.js Neural Network"
-            description="Description"
-          />
+          <ProjectBox title="BodyPose" description="Description" />
+          <ProjectBox title="HandPose" description="Description" />
+          <ProjectBox title="FaceMesh" description="Description" />
+          <ProjectBox title="Image Classifier" description="Description" />
+          <ProjectBox title="Sound Classifier" description="Description" />
+          <ProjectBox title="ml5.js Neural Network" description="Description" />
         </div>
         <Spacer height="3rem" />
       </section>
 
       <section aria-label="Featured Projects">
-        <h2>
-          Discover the creative possibilities of machine learning!
-        </h2>
-        <p>
-          Coming soon!
-        </p>
+        <h2>Discover the creative possibilities of machine learning!</h2>
+        <p>Coming soon!</p>
         {/* <div style={styles.flexContainer}>
           {posts.map((post) => (
             // <div key={post.id}></div>
@@ -94,19 +68,17 @@ const IndexPage = ({ data }) => {
         </div> */}
       </section>
     </Layout>
-  )
-}
+  );
+};
 
-export const Head = () => <title>ml5.js | Friendly Machine Learning for the Web</title>
+export const Head = () => <title>ml5.js | Friendly Machine Learning for the Web</title>;
 
 export default IndexPage;
 
 // GraphQL
 export const query = graphql`
   query CommunityQuery {
-    allMarkdownRemark(
-      filter: { frontmatter: { featuredPost: { eq: true } } }
-    ) {
+    allMarkdownRemark(filter: { frontmatter: { featuredPost: { eq: true } } }) {
       nodes {
         id
         frontmatter {
@@ -119,4 +91,4 @@ export const query = graphql`
       }
     }
   }
-`
+`;
--- src/pages/learn.js
+++ src/pages/learn.js
@@ -1,21 +1,19 @@
-import * as React from "react";
-import Layout from "../layout/Layout";
-import Spacer from "../components/Spacer";
-import IframeComponent from "../components/IframeComponent";
+import * as React from 'react';
+import Layout from '../layout/Layout';
+import Spacer from '../components/Spacer';
+import IframeComponent from '../components/IframeComponent';
 
 const LearnPage = () => {
   return (
     <Layout>
       <IframeComponent />
       <h1>Learn</h1>
-      <p>
-        Coming Soon!
-      </p>
+      <p>Coming Soon!</p>
       <Spacer />
     </Layout>
-  )
-}
+  );
+};
 
-export const Head = () => <title>ml5.js | Friendly Machine Learning for the Web</title>
+export const Head = () => <title>ml5.js | Friendly Machine Learning for the Web</title>;
 
-export default LearnPage
+export default LearnPage;
