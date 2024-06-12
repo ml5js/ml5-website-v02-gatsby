093c780b71c8bd28ad5f8f262114ceea5eb45c1a
Alan Ren <10086000+alanvww@users.noreply.github.com>
2024-06-12 15:53:10 -0400

    Update style and add lazyloading to About

--- src/components/IframeBgComponent.js
+++ src/components/IframeBgComponent.js
@@ -1,33 +1,33 @@
 import React from 'react';
 
 const IframeBgComponent = (style) => {
-  const styles = {
-    container: {
-      position: "absolute",
-      top: 0,
-      left: 0,
-      width: "100vw",
-      height: "100vh",
-      overflow: "hidden",
-      zIndex: -1,
-    },
-    iframe: {
-      width: "100%",
-      height: "100%",
-      border: "none",
-    },
-  };
+	const styles = {
+		container: {
+			position: 'absolute',
+			top: 0,
+			left: 0,
+			width: '100vw',
+			height: '100vh',
+			overflow: 'hidden',
+			zIndex: -1,
+		},
+		iframe: {
+			width: '100vw',
+			height: '100%',
+			border: 'none',
+		},
+	};
 
-  return (
-    <div style={{ ...styles.container, ...style }}>
-      <iframe
-        src="https://moqn.github.io/ml5-website-hero/sketch/"
-        title="Hero Sketch"
-        style={styles.iframe}
-        allowFullScreen
-      ></iframe>
-    </div>
-  );
+	return (
+		<div style={styles.container}>
+			<iframe
+				src="https://moqn.github.io/ml5-website-hero/sketch/"
+				title="Hero Sketch"
+				style={styles.iframe}
+				allowFullScreen
+			></iframe>
+		</div>
+	);
 };
 
 export default IframeBgComponent;
--- src/components/IframeComponent.js
+++ src/components/IframeComponent.js
@@ -1,29 +1,29 @@
 import React from 'react';
 
 const IframeComponent = () => {
-  const styles = {
-    container: {
-      width: "100%",
-      height: "22rem",
-      overflow: "hidden",
-    },
-    iframe: {
-      width: "100%",
-      height: "100%",
-      border: "none",
-    },
-  };
+	const styles = {
+		container: {
+			width: '100vw',
+			height: '22rem',
+			overflow: 'hidden',
+		},
+		iframe: {
+			width: '100%',
+			height: '100%',
+			border: 'none',
+		},
+	};
 
-  return (
-    <div style={styles.container}>
-      <iframe
-        src="https://moqn.github.io/ml5-website-hero/sketch/"
-        title="Hero Sketch"
-        style={styles.iframe}
-        allowFullScreen
-      ></iframe>
-    </div>
-  );
+	return (
+		<div style={styles.container}>
+			<iframe
+				src="https://moqn.github.io/ml5-website-hero/sketch/"
+				title="Hero Sketch"
+				style={styles.iframe}
+				allowFullScreen
+			></iframe>
+		</div>
+	);
 };
 
 export default IframeComponent;
