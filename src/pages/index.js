f6c5658a5ee14f66622bb9a6b86131c628e13a88
MOQN <moonjunghyun@gmail.com>
2025-03-10 02:44:12 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,7 +57,7 @@ const IndexPage = ({ data }) => {
 					content="ml5.js aims to make machine learning approachable for a broad audience of artists, creative coders, and students. The library provides access to machine learning algorithms and models in the browser, building on top of TensorFlow.js with no other external dependencies."
 					maxWidth="85%"
 				/>
-				<Spacer height="6rem" />
+				<Spacer height="5rem" />
 
 				<div style={styles.flexContainer}>
 					<h1>What we can do with ml5.js?</h1>
